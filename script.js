36484cac00f9f692ab82f172963c62505cb50d18
LenziFire1 <oleksandrgladcenko33@gmail.com>
2024-12-14 18:17:13 +0200

    Week, mouns, year, JS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let day = document.querySelector(".day");
+let week = document.querySelector(".week");
+let mouns = document.querySelector(".mouns");
+let year = document.querySelector(".year");
+
+day.innerHTML = "12";
+week.innerHTML = "Субота";
+mouns.innerHTML = "Грудень";
+year.innerHTML = "2045";
+
+let today = new Date();
+console.log(today)
+
+day.innerHTML = today.getDate()
+week.innerHTML = today.getDay()
+mouns.innerHTML = today.getMonth()
+year.innerHTML = today.getFullYear()
 
