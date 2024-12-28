79040ef1d61cacb31bacba63b1ebd45c9357df4b
LenziFire1 <sashalotar@gmail.com>
2024-12-28 18:02:01 +0200

    finished

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ let day = document.querySelector(".day");
 let week = document.querySelector(".week");
 let mouns = document.querySelector(".mouns");
 let year = document.querySelector(".year");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
 
-day.innerHTML = "12";
-week.innerHTML = "Субота";
-mouns.innerHTML = "Грудень";
-year.innerHTML = "2045";
+// day.innerHTML = "12";
+// week.innerHTML = "Субота";
+// mouns.innerHTML = "Грудень";
+// year.innerHTML = "2045";
+// hours.innerHTML = "11";
+
+
+
+let days = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
+let mounss = ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"];
 
 let today = new Date();
 console.log(today)
 
 day.innerHTML = today.getDate()
-week.innerHTML = today.getDay()
-mouns.innerHTML = today.getMonth()
+week.innerHTML = days[today.getDay()]
+mouns.innerHTML = mounss[today.getMonth()]
 year.innerHTML = today.getFullYear()
+hours.innerHTML = today.getHours()
+minutes.innerHTML = today.getMinutes()
+seconds.innerHTML = today.getSeconds()
 
+setInterval(() =>{
+    let currentTime = new Date();
+    hours.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    minutes.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    seconds.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000)
